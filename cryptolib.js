f7a5eac573cf888bf540f4bc8c6e0d3cd1f48717
Kli0 <klio.inform@gmail.com>
2023-09-16 23:24:48 +0700

    implement Diffie-Hellman algorithm; fix cryptoPow function

--- cryptolib.js
+++ cryptolib.js
@@ -9,17 +9,28 @@ function cryptoPow(base, exp, mod) {
             result = result * (base % mod);
             exp = exp - 1;
         } else {
-            base = base * base
+            base = base * (base % mod);
             exp >>= 1
         }
     }
-    return result;
+    return (result % mod);
 }
 
-let a = 5;
-let b = 11;
-let c = 3;
-console.log(cryptoPow(a, b, c));
+//https://prog-cpp.ru/pow-mod/
+// function cryptoPow(base, exp, mod) {
+//     if (exp == 0) return 1;
+//     let z = cryptoPow(base, Math.floor(exp / 2), mod);
+//     if (exp % 2 == 0) {
+//         return (z * z) % mod;
+//     } else {
+//         return ((base % mod) * ((z * z) % mod)) % mod;
+//     }
+// }
+
+// let a = 5;
+// let b = 11;
+// let c = 3;
+// console.log(cryptoPow(a, b, c));
 
 // Расширенный алгоритм Евклида (НОД + коэффициенты)
 function cryptoGCD(a, b, coef) {
@@ -35,9 +46,77 @@ function cryptoGCD(a, b, coef) {
     return d;
 }
 
-let num1 = 35;
-let num2 = 67;
-let coef = {x: 0, y: 0};
-console.log(cryptoGCD(num1, num2, coef));
-console.log(coef.x);
-console.log(coef.y);
+// let num1 = 35;
+// let num2 = 67;
+// let coef = {x: 0, y: 0};
+// console.log(cryptoGCD(num1, num2, coef));
+// console.log(coef.x);
+// console.log(coef.y);
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}  
+
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    let sq = Math.sqrt(number);
+    for (let index = 2; index <= sq; index++) {
+        if (number % index == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function generatePrime() {
+    let randNum;
+    do {
+        randNum = generateRandomNumber(1, 100);
+    } while (!isPrime(randNum));
+    return randNum;
+}
+
+function generateCommonKey() {
+    // Простой модуль
+    let p = generatePrime();
+    console.log(`p: ${p}`);
+    // База (генератор)
+    let g = generatePrime();
+    console.log(`g: ${g}`);
+
+    // Приватная часть генерации общего ключа Элис
+    let privateKeyAlice;
+    do {
+        privateKeyAlice = generateRandomNumber(1, 100)
+    } while (privateKeyAlice >= p);
+    console.log(`Alice private key: ${privateKeyAlice}`);
+    // Приватная часть генерации общего ключа Боба
+    let privateKeyBob;
+    do {
+        privateKeyBob = generateRandomNumber(1, 100)
+    } while (privateKeyBob >= p);
+    console.log(`Bob private key: ${privateKeyBob}`);
+
+    // Ошибка в функции cryptoPow - возвращает числа больше модуля??????????
+    let publicKeyAlice = cryptoPow(g, privateKeyAlice, p);
+    console.log(`Alice public key: ${publicKeyAlice}`);
+    let publicKeyBob = cryptoPow(g, privateKeyBob, p);
+    console.log(`Bob public key: ${publicKeyBob}`);
+
+
+    let commonKeyAlice = cryptoPow(publicKeyBob, privateKeyAlice, p);
+    let commonKeyBob = cryptoPow(publicKeyAlice, privateKeyBob, p);
+
+    console.log(commonKeyAlice);
+    console.log(commonKeyBob);
+}
+
+generateCommonKey();
+
+// console.log(isPrime(7));
+// console.log(isPrime(12512));
+// console.log(isPrime(3563412));
+// console.log(generatePrime());
+// console.log(generatePrime());
